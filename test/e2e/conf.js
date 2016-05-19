15d57088184e4fa727f52bdc45118a20aece3b1d
William Bautista <williambautista@gmail.com>
2016-05-19 15:20:43 +0100

    node server added

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,6 +1,22 @@
 exports.config = {
   framework: 'jasmine',
+
   seleniumAddress: 'http://localhost:4444/wd/hub',
+
   capabilities: { 'browserName': 'chrome' },
-  specs: ['githubProfileFeature.js']
+
+  specs: ['githubProfileFeature.js'],
+
+  jasmineNodeOpts: {
+    showColors: true
+  },
+
+  onPrepare: function() {
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.TerminalReporter({
+      verbosity: 3,
+      color: true,
+      showStack: true
+    }));
+  }
 };
