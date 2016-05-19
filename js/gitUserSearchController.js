b0e275386df3c98ae22d7e860025a1e7094b30b3
William Bautista <williambautista@gmail.com>
2016-05-19 08:28:16 +0100

    Search box validation added

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,11 +2,12 @@ githubUserSearch.controller('GitUserSearchController',
                 ['$resource', function($resource) {
 
   var self = this;
+
   var searchResource = $resource('https://api.github.com/search/users');
 
   self.doSearch = function() {
-    self.searchResult = searchResource.get(
-      { q: self.searchTerm}
-    );
+      self.searchResult = searchResource.get({
+        q: self.searchTerm
+      });
   };
 }]);
