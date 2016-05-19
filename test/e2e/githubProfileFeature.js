ef816318c4aa982c7ae82ed98874622a9c69a891
William Bautista <williambautista@gmail.com>
2016-05-19 15:35:49 +0100

    Feature test added

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,21 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
 
+  var searchBox = element(by.model('searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github Profiles');
+  });
+
+  it('retrieves profiles', function() {
+    searchBox.sendKeys('Williba');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.itmes'));
+    expect(profiles.getText()).toContain('Willibaur');
   });
 });
